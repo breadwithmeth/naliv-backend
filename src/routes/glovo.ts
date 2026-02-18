2b0f3c8c5cebd64deeee2b2ca41632f7d54b7973
shrvse <sergeq2222@gmail.com>
2026-02-18 20:24:19 +0500

    +

--- src/routes/glovo.ts
+++ src/routes/glovo.ts
@@ -4,12 +4,12 @@ import glovoController from '../controllers/glovoController'
 const router = Router()
 
 // POST /api/glovo/assortment
-router.post('/assortment', glovoController.sendAssortment)
+//router.post('/assortment', glovoController.sendAssortment)
 
 // POST /api/glovo/stores/:storeId/menu/updates
-router.post('/stores/:storeId/menu/updates', glovoController.bulkUpdate)
+//outer.post('/stores/:storeId/menu/updates', glovoController.bulkUpdate)
 
 // POST /api/glovo/menu/update-from-db?business_id=123
-router.post('/menu/update-from-db', glovoController.updateMenusFromDb)
+//router.post('/menu/update-from-db', glovoController.updateMenusFromDb)
 
 export default router
